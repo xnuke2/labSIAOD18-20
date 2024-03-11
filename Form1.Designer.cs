3ec48ca4148e78ff2be2ccf0b2945e9da784253b
xnuke2 <xmnikit@gmail.com>
2024-03-11 20:01:20 +0300

    Добавлены методы Print и Clear_Tab а также их тестирование

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,54 +40,51 @@
             label2 = new Label();
             panel1 = new Panel();
             panel2 = new Panel();
+            buttonExit = new Button();
+            dataGridViewRezult = new DataGridView();
+            label5 = new Label();
+            dataGridViewTree = new DataGridView();
+            label4 = new Label();
+            dataGridViewArray = new DataGridView();
             label3 = new Label();
-            dataGridView1 = new DataGridView();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
-            Column4 = new DataGridViewTextBoxColumn();
-            Column5 = new DataGridViewTextBoxColumn();
-            Column6 = new DataGridViewTextBoxColumn();
-            Column7 = new DataGridViewTextBoxColumn();
-            Column8 = new DataGridViewTextBoxColumn();
-            Column9 = new DataGridViewTextBoxColumn();
-            Column10 = new DataGridViewTextBoxColumn();
-            Column11 = new DataGridViewTextBoxColumn();
-            Column12 = new DataGridViewTextBoxColumn();
-            Column13 = new DataGridViewTextBoxColumn();
-            Column14 = new DataGridViewTextBoxColumn();
-            Column15 = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)numericUpDownNewElement).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDownChangePiorityOld).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDownChangePiorityNew).BeginInit();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewRezult).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTree).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewArray).BeginInit();
             SuspendLayout();
             // 
             // buttonCreateQueue
             // 
-            buttonCreateQueue.Location = new Point(3, 13);
+            buttonCreateQueue.Location = new Point(23, 10);
+            buttonCreateQueue.Margin = new Padding(3, 2, 3, 2);
             buttonCreateQueue.Name = "buttonCreateQueue";
-            buttonCreateQueue.Size = new Size(192, 29);
+            buttonCreateQueue.Size = new Size(168, 22);
             buttonCreateQueue.TabIndex = 0;
             buttonCreateQueue.Text = "Создать очередь";
             buttonCreateQueue.UseVisualStyleBackColor = true;
+            buttonCreateQueue.Click += buttonCreateQueue_Click;
             // 
             // buttonClearQueue
             // 
-            buttonClearQueue.Location = new Point(239, 13);
+            buttonClearQueue.Location = new Point(287, 10);
+            buttonClearQueue.Margin = new Padding(3, 2, 3, 2);
             buttonClearQueue.Name = "buttonClearQueue";
-            buttonClearQueue.Size = new Size(192, 29);
+            buttonClearQueue.Size = new Size(168, 22);
             buttonClearQueue.TabIndex = 1;
             buttonClearQueue.Text = "Очистить очередь";
             buttonClearQueue.UseVisualStyleBackColor = true;
+            buttonClearQueue.Click += buttonClearQueue_Click;
             // 
             // buttoтExtractMax
             // 
-            buttoтExtractMax.Location = new Point(463, 13);
+            buttoтExtractMax.Location = new Point(550, 10);
+            buttoтExtractMax.Margin = new Padding(3, 2, 3, 2);
             buttoтExtractMax.Name = "buttoтExtractMax";
-            buttoтExtractMax.Size = new Size(192, 29);
+            buttoтExtractMax.Size = new Size(168, 22);
             buttoтExtractMax.TabIndex = 2;
             buttoтExtractMax.Text = "Извлечь наибольший";
             buttoтExtractMax.UseVisualStyleBackColor = true;
@@ -95,58 +92,63 @@
             // 
             // buttonPasteNew
             // 
-            buttonPasteNew.Location = new Point(3, 61);
+            buttonPasteNew.Location = new Point(23, 48);
+            buttonPasteNew.Margin = new Padding(3, 2, 3, 2);
             buttonPasteNew.Name = "buttonPasteNew";
-            buttonPasteNew.Size = new Size(192, 29);
+            buttonPasteNew.Size = new Size(168, 22);
             buttonPasteNew.TabIndex = 3;
             buttonPasteNew.Text = "Вставить новый";
             buttonPasteNew.UseVisualStyleBackColor = true;
             // 
             // buttonChangePriority
             // 
-            buttonChangePriority.Location = new Point(315, 61);
+            buttonChangePriority.Location = new Point(356, 50);
+            buttonChangePriority.Margin = new Padding(3, 2, 3, 2);
             buttonChangePriority.Name = "buttonChangePriority";
-            buttonChangePriority.Size = new Size(192, 29);
+            buttonChangePriority.Size = new Size(168, 22);
             buttonChangePriority.TabIndex = 4;
             buttonChangePriority.Text = "Изменить приоритет";
             buttonChangePriority.UseVisualStyleBackColor = true;
             // 
             // numericUpDownNewElement
             // 
-            numericUpDownNewElement.Location = new Point(201, 63);
+            numericUpDownNewElement.Location = new Point(196, 49);
+            numericUpDownNewElement.Margin = new Padding(3, 2, 3, 2);
             numericUpDownNewElement.Name = "numericUpDownNewElement";
-            numericUpDownNewElement.Size = new Size(75, 27);
+            numericUpDownNewElement.Size = new Size(66, 23);
             numericUpDownNewElement.TabIndex = 5;
             // 
             // numericUpDownChangePiorityOld
             // 
-            numericUpDownChangePiorityOld.Location = new Point(548, 63);
+            numericUpDownChangePiorityOld.Location = new Point(550, 50);
+            numericUpDownChangePiorityOld.Margin = new Padding(3, 2, 3, 2);
             numericUpDownChangePiorityOld.Name = "numericUpDownChangePiorityOld";
-            numericUpDownChangePiorityOld.Size = new Size(75, 27);
+            numericUpDownChangePiorityOld.Size = new Size(66, 23);
             numericUpDownChangePiorityOld.TabIndex = 6;
             // 
             // numericUpDownChangePiorityNew
             // 
-            numericUpDownChangePiorityNew.Location = new Point(661, 63);
+            numericUpDownChangePiorityNew.Location = new Point(648, 50);
+            numericUpDownChangePiorityNew.Margin = new Padding(3, 2, 3, 2);
             numericUpDownChangePiorityNew.Name = "numericUpDownChangePiorityNew";
-            numericUpDownChangePiorityNew.Size = new Size(75, 27);
+            numericUpDownChangePiorityNew.Size = new Size(66, 23);
             numericUpDownChangePiorityNew.TabIndex = 7;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(526, 65);
+            label1.Location = new Point(530, 52);
             label1.Name = "label1";
-            label1.Size = new Size(16, 20);
+            label1.Size = new Size(13, 15);
             label1.TabIndex = 8;
             label1.Text = "c";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(629, 65);
+            label2.Location = new Point(620, 52);
             label2.Name = "label2";
-            label2.Size = new Size(26, 20);
+            label2.Size = new Size(20, 15);
             label2.TabIndex = 9;
             label2.Text = "на";
             // 
@@ -163,158 +165,116 @@
             panel1.Controls.Add(numericUpDownChangePiorityOld);
             panel1.Controls.Add(buttonChangePriority);
             panel1.Controls.Add(numericUpDownNewElement);
-            panel1.Location = new Point(12, 12);
+            panel1.Location = new Point(10, 9);
+            panel1.Margin = new Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(776, 114);
+            panel1.Size = new Size(761, 86);
             panel1.TabIndex = 10;
             // 
             // panel2
             // 
             panel2.BackColor = SystemColors.GradientActiveCaption;
-            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(buttonExit);
+            panel2.Controls.Add(dataGridViewRezult);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(dataGridViewTree);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(dataGridViewArray);
             panel2.Controls.Add(label3);
-            panel2.Location = new Point(12, 132);
+            panel2.Location = new Point(10, 99);
+            panel2.Margin = new Padding(3, 2, 3, 2);
             panel2.Name = "panel2";
-            panel2.Size = new Size(776, 306);
+            panel2.Size = new Size(761, 278);
             panel2.TabIndex = 11;
+            // 
+            // buttonExit
+            // 
+            buttonExit.Location = new Point(638, 238);
+            buttonExit.Name = "buttonExit";
+            buttonExit.Size = new Size(119, 37);
+            buttonExit.TabIndex = 6;
+            buttonExit.Text = "Выход";
+            buttonExit.UseVisualStyleBackColor = true;
+            buttonExit.Click += buttonExit_Click;
+            // 
+            // dataGridViewRezult
+            // 
+            dataGridViewRezult.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewRezult.ColumnHeadersVisible = false;
+            dataGridViewRezult.Location = new Point(4, 209);
+            dataGridViewRezult.Margin = new Padding(3, 2, 3, 2);
+            dataGridViewRezult.Name = "dataGridViewRezult";
+            dataGridViewRezult.RowHeadersVisible = false;
+            dataGridViewRezult.RowHeadersWidth = 50;
+            dataGridViewRezult.Size = new Size(754, 24);
+            dataGridViewRezult.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label5.Location = new Point(315, 192);
+            label5.Name = "label5";
+            label5.Size = new Size(118, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Результат выборки";
+            // 
+            // dataGridViewTree
+            // 
+            dataGridViewTree.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewTree.ColumnHeadersVisible = false;
+            dataGridViewTree.Location = new Point(3, 87);
+            dataGridViewTree.Margin = new Padding(3, 2, 3, 2);
+            dataGridViewTree.Name = "dataGridViewTree";
+            dataGridViewTree.RowHeadersVisible = false;
+            dataGridViewTree.RowHeadersWidth = 50;
+            dataGridViewTree.Size = new Size(754, 103);
+            dataGridViewTree.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label4.Location = new Point(278, 61);
+            label4.Name = "label4";
+            label4.Size = new Size(183, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Представление в виде дерева";
+            // 
+            // dataGridViewArray
+            // 
+            dataGridViewArray.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewArray.ColumnHeadersVisible = false;
+            dataGridViewArray.Location = new Point(3, 24);
+            dataGridViewArray.Margin = new Padding(3, 2, 3, 2);
+            dataGridViewArray.Name = "dataGridViewArray";
+            dataGridViewArray.RowHeadersVisible = false;
+            dataGridViewArray.RowHeadersWidth = 50;
+            dataGridViewArray.Size = new Size(754, 24);
+            dataGridViewArray.TabIndex = 1;
+            dataGridViewArray.CellContentClick += dataGridView1_CellContentClick;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label3.Location = new Point(271, 9);
+            label3.Location = new Point(278, 7);
             label3.Name = "label3";
-            label3.Size = new Size(236, 20);
+            label3.Size = new Size(190, 15);
             label3.TabIndex = 0;
             label3.Text = "Представление в виде массива";
             // 
-            // dataGridView1
-            // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.ColumnHeadersVisible = false;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6, Column7, Column8, Column9, Column10, Column11, Column12, Column13, Column14, Column15 });
-            dataGridView1.Location = new Point(3, 32);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersVisible = false;
-            dataGridView1.RowHeadersWidth = 50;
-            dataGridView1.Size = new Size(770, 32);
-            dataGridView1.TabIndex = 1;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
-            // 
-            // Column1
-            // 
-            Column1.HeaderText = "Column1";
-            Column1.MinimumWidth = 6;
-            Column1.Name = "Column1";
-            Column1.Width = 50;
-            // 
-            // Column2
-            // 
-            Column2.HeaderText = "Column2";
-            Column2.MinimumWidth = 6;
-            Column2.Name = "Column2";
-            Column2.Width = 50;
-            // 
-            // Column3
-            // 
-            Column3.HeaderText = "Column3";
-            Column3.MinimumWidth = 6;
-            Column3.Name = "Column3";
-            Column3.Width = 50;
-            // 
-            // Column4
-            // 
-            Column4.HeaderText = "Column4";
-            Column4.MinimumWidth = 6;
-            Column4.Name = "Column4";
-            Column4.Width = 50;
-            // 
-            // Column5
-            // 
-            Column5.HeaderText = "Column5";
-            Column5.MinimumWidth = 6;
-            Column5.Name = "Column5";
-            Column5.Width = 50;
-            // 
-            // Column6
-            // 
-            Column6.HeaderText = "Column6";
-            Column6.MinimumWidth = 6;
-            Column6.Name = "Column6";
-            Column6.Width = 50;
-            // 
-            // Column7
-            // 
-            Column7.HeaderText = "Column7";
-            Column7.MinimumWidth = 6;
-            Column7.Name = "Column7";
-            Column7.Width = 50;
-            // 
-            // Column8
-            // 
-            Column8.HeaderText = "Column8";
-            Column8.MinimumWidth = 6;
-            Column8.Name = "Column8";
-            Column8.Width = 50;
-            // 
-            // Column9
-            // 
-            Column9.HeaderText = "Column9";
-            Column9.MinimumWidth = 6;
-            Column9.Name = "Column9";
-            Column9.Width = 50;
-            // 
-            // Column10
-            // 
-            Column10.HeaderText = "Column10";
-            Column10.MinimumWidth = 6;
-            Column10.Name = "Column10";
-            Column10.Width = 50;
-            // 
-            // Column11
-            // 
-            Column11.HeaderText = "Column11";
-            Column11.MinimumWidth = 6;
-            Column11.Name = "Column11";
-            Column11.Width = 50;
-            // 
-            // Column12
-            // 
-            Column12.HeaderText = "Column12";
-            Column12.MinimumWidth = 6;
-            Column12.Name = "Column12";
-            Column12.Width = 50;
-            // 
-            // Column13
-            // 
-            Column13.HeaderText = "Column13";
-            Column13.MinimumWidth = 6;
-            Column13.Name = "Column13";
-            Column13.Width = 50;
-            // 
-            // Column14
-            // 
-            Column14.HeaderText = "Column14";
-            Column14.MinimumWidth = 6;
-            Column14.Name = "Column14";
-            Column14.Width = 50;
-            // 
-            // Column15
-            // 
-            Column15.HeaderText = "Column15";
-            Column15.MinimumWidth = 6;
-            Column15.Name = "Column15";
-            Column15.Width = 50;
-            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(783, 388);
             Controls.Add(panel2);
             Controls.Add(panel1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Лабораторная работа 18-10 Хмыренков";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)numericUpDownNewElement).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDownChangePiorityOld).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDownChangePiorityNew).EndInit();
@@ -322,7 +282,9 @@
             panel1.PerformLayout();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewRezult).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTree).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewArray).EndInit();
             ResumeLayout(false);
         }
 
@@ -340,22 +302,12 @@
         private Label label2;
         private Panel panel1;
         private Panel panel2;
-        private DataGridView dataGridView1;
+        private DataGridView dataGridViewArray;
         private Label label3;
-        private DataGridViewTextBoxColumn Column1;
-        private DataGridViewTextBoxColumn Column2;
-        private DataGridViewTextBoxColumn Column3;
-        private DataGridViewTextBoxColumn Column4;
-        private DataGridViewTextBoxColumn Column5;
-        private DataGridViewTextBoxColumn Column6;
-        private DataGridViewTextBoxColumn Column7;
-        private DataGridViewTextBoxColumn Column8;
-        private DataGridViewTextBoxColumn Column9;
-        private DataGridViewTextBoxColumn Column10;
-        private DataGridViewTextBoxColumn Column11;
-        private DataGridViewTextBoxColumn Column12;
-        private DataGridViewTextBoxColumn Column13;
-        private DataGridViewTextBoxColumn Column14;
-        private DataGridViewTextBoxColumn Column15;
+        private Label label4;
+        private DataGridView dataGridViewTree;
+        private DataGridView dataGridViewRezult;
+        private Label label5;
+        private Button buttonExit;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,100 @@ namespace labSIAOD18_20
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridViewArray.RowCount = 1;
+            dataGridViewArray.ColumnCount = 15;
+            for (int i = 0; i < dataGridViewArray.ColumnCount; i++)
+            {
+                dataGridViewArray.Columns[i].Width = Convert.ToInt32(dataGridViewArray.Width / dataGridViewArray.ColumnCount - 0.2);
+
+            }
+            dataGridViewTree.RowCount = 4;
+            dataGridViewTree.ColumnCount = 15;
+            for (int i = 0; i < dataGridViewTree.ColumnCount; i++)
+            {
+                dataGridViewTree.Columns[i].Width = Convert.ToInt32(dataGridViewTree.Width / dataGridViewTree.ColumnCount - 0.2);
+            }
+            dataGridViewRezult.RowCount = 1;
+            dataGridViewRezult.ColumnCount = 15;
+            for (int i = 0; i < dataGridViewRezult.ColumnCount; i++)
+            {
+                dataGridViewRezult.Columns[i].Width = Convert.ToInt32(dataGridViewRezult.Width / dataGridViewRezult.ColumnCount - 0.2);
+            }
+        }
+        void Print(int[] array)
+        {
+            int i = 0;
+            int j = 3;
+            int j1 = 1;
+            int j2 = 0;
+            int numOfIter = 0;
+            while (i < array.Length)
+            {
+                while (array[i] == 0)
+                {
+                    i++;
+                    if (i >= array.Length) { return; }
+                }
+                switch (numOfIter)
+                {
+                    case 0:
+                        dataGridViewTree.Rows[0].Cells[7].Value = array[i];
+                        break;
+                    case <= 2:
+                        dataGridViewTree.Rows[1].Cells[j].Value = array[i];
+                        j += 8;
+                        break;
+                    case <= 6:
+                        dataGridViewTree.Rows[2].Cells[j1].Value = array[i];
+                        j1 += 4;
+                        break;
+                    case <= 14:
+                        dataGridViewTree.Rows[3].Cells[j2].Value = array[i];
+                        j2 += 2;
+                        break;
+                }
+                i++;
+                numOfIter++;
+            }
+        }
+        void Clear_Tab()
+        {
+            for (int i = 0; i < dataGridViewArray.Rows[0].Cells.Count; i++)
+            {
+                dataGridViewArray.Rows[0].Cells[i].Value = "";
+            }
+            for (int j = 0; j < dataGridViewTree.Rows.Count; j++)
+                for (int i = 0; i < dataGridViewTree.Rows[0].Cells.Count; i++)
+                    dataGridViewTree.Rows[j].Cells[i].Value = "";
+            for (int i = 0; i < dataGridViewRezult.Rows[0].Cells.Count; i++)
+            {
+                dataGridViewRezult.Rows[0].Cells[i].Value = "";
+            }
+        }
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonCreateQueue_Click(object sender, EventArgs e)
+        {
+            Clear_Tab();
+            int[] arr = new int[15];
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(10, 99);
+                dataGridViewArray.Rows[0].Cells[i].Value = arr[i];
+            }
+            Print(arr);
+        }
+
+        private void buttonClearQueue_Click(object sender, EventArgs e)
+        {
+            Clear_Tab();
+        }
     }
 }
